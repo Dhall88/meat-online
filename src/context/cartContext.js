e73d076ae2080d704f783d2571e7b1fd51471634
David Hall <davidchall388@gmail.com>
2021-03-25 11:27:14 -0400

    sorting state persists through reload, restructured router

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,28 +1,43 @@
 import React, { useReducer, createContext } from "react";
+import Cookies from 'universal-cookie'
 
 export const CartContext = createContext();
 
+
+const cookies = new Cookies();
+
 const initialState = {
-  products: [],
+  products: cookies.get('cartState'),
   loading: false,
   error: null
 };
 
+
+
+
 console.log("in cart context")
+let result;
 
 const reducer = (state, action) => {
   console.log(action.payload)
   switch (action.type) {
     case "ADD_PRODUCT":
+
+      
+      result = [...state.products, action.payload]
+      cookies.set('cartState', result, { path: '/' });
       return {
-        products: [...state.products, action.payload]
+        products: result
       };
+
     case "DEL_PRODUCT":
+      result = state.products.filter((product) => {
+        return product._id !== action.payload._id
+      }
+      )
+      cookies.set('cartState', result, { path: '/' });
       return {
-        products: state.products.filter((product) => {
-          return product._id !== action.payload._id
-        }
-        )
+        products: result
       };
     case "START":
       return {
--- src/context/sortingContext.js
+++ src/context/sortingContext.js
@@ -1,21 +1,42 @@
 import React, { useReducer, createContext } from "react";
+import Cookies from "universal-cookie"
 
 export const SortingContext = createContext();
 
+const cookies = new Cookies();
+
 const initialState = {
-  sorting: 'default',
+    category: cookies.get("sortingState").category,
+  sorting: cookies.get("sortingState").sorting,
   loading: false,
   error: null
 };
 
+let result;
+
 
 const reducer = (state, action) => {
-  console.log(action.payload)
   switch (action.type) {
     case "SET_SORTING":
+        result = {category: state.category,
+                    sorting: action.payload
+        }
+        cookies.set('sortingState', result, { path: '/' });
       return {
-        sorting: action.payload
+          result
+
       };
+    case "SET_CATEGORY":
+        
+        result = {category: action.payload,
+                sorting: state.sorting
+}
+
+cookies.set('sortingState', result, { path: '/' });
+return (
+    result
+);
+      
     case "START":
       return {
         loading: true
